95e343d9a0ec10babebb4a9804103fa315054d44
jonathan canaveral <neumerance@live.com>
2019-11-15 20:34:30 +0800

    logo field for both education and work experience

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,7 @@
 class Entity < ApplicationRecord
   belongs_to :resume
 
-  has_attached_file :logo, styles: { passport: '400x400>' }, default_url: "/images/:style/missing.png"
+  has_attached_file :logo, styles: { passport: '400x400>' }, default_url: 'http://via.placeholder.com/300x300'
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
   validates :name, :title, :since, :type, presence: true
