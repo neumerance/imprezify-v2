4058c0886232c1e9d3b5e0516719a7b96097ac85
jonathan canaveral <neumerance@live.com>
2019-11-23 23:20:41 +0800

    seed plus improvements in skill section

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = FactoryBot.create(:user, email: 'user@imprezify.com')
+resume = FactoryBot.create(:resume, user: user)
+FactoryBot.create(:work_experience, resume: resume)
+FactoryBot.create_list(:education, 3, resume: resume)
+FactoryBot.create_list(:skill, 5, resume: resume)
