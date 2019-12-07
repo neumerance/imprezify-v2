c343b72ff533a75466055cc0cca6eecefc000c41
jonathan canaveral <neumerance@live.com>
2019-12-07 09:26:30 +0800

    implement separate form and controller for work_experience crud

--- app/helpers/resume_helper.rb
+++ app/helpers/resume_helper.rb
@@ -1,22 +1,22 @@
 module ResumeHelper
-  def add_more_field_button(model, attr)
+  def add_more_field_button(model, name, attr)
     link_to('javascript:void(0)',
-            'data-object-name' => "#{model.object_name}[#{attr}][]",
+            'data-object-name' => name,
             'onclick' => "addMoreField(this)") do
       concat content_tag(:span, '', class: 'fa fa-plus-circle mr-2')
-      concat content_tag(:span, model.object.try(attr).any? ? 'Add more description' : 'Add Descriptions')
+      concat content_tag(:span, model.try(attr).any? ? 'Add more description' : 'Add Descriptions')
     end
   end
 
-  def image_field(model, attr)
-    name = model.object_name.parameterize.underscore + '_' + attr.to_s
+  def image_field(model, name, attr)
+    id = name.parameterize.underscore
     content_tag(:div, class: 'position-absolute') do
-      concat model.file_field attr, class: 'd-none', data: {behaviour: 'image-input', 'image-tag' => name}, onchange: 'submitForm(this)'
-      concat image_tag model.object.try(attr).try(:url), id: name + '_preview', class: 'rounded d-block width-100'
+      concat file_field_tag name, class: 'd-none', data: { behaviour: 'image-input', 'image-tag' => id }, onchange: 'submitForm(this)'
+      concat image_tag model.try(attr).try(:url), id: id + '_preview', class: 'rounded d-block width-100'
       concat link_to(content_tag(:span, ' Change', class: 'fa fa-pen-square'),
                      'javascript:void(0)',
                      class: 'text-white rounded py-1 px-2 change-photo position-relative',
-                     onclick: "document.getElementById('#{name}').click()")
+                     onclick: "document.querySelectorAll('input[name=\"#{name}\"]')[0].click()")
     end
   end
 
