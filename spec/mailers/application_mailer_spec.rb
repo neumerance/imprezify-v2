43e22b6ca92d96c74ceb2942cbdb3e62836ec34c
jonathan canaveral <neumerance@live.com>
2020-01-25 17:44:12 +0800

    pdf generator mailer spec

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 describe ApplicationMailer do
+  let(:url) { 'http://www.google.com' }
+  let(:resume) { create(:resume) }
 
+  before { mock_api_2_pdf }
+
+  describe '#send_generated_pdf' do
+    subject { ApplicationMailer.send_generated_pdf(resume) }
+
+    it 'sends an email' do
+      expect { subject.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    context 'mail content' do
+      let(:mail) { subject.deliver }
+      let(:attachments) { mail.attachments }
+
+      it 'has right contents' do
+        expect(mail.from).to include 'info@imprezify.com'
+        expect(mail.to).to include resume.user.email
+        expect(mail.subject).to eq 'Imprezify - Thank you for downloading'
+      end
+
+      it 'attaches pdf' do
+        expect(attachments.any?).to be_truthy
+        expect(attachments.first.content_type).to eq 'application/pdf'
+      end
+    end
+  end
 end
