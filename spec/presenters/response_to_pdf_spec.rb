00ce9879c6877457b67f9b6b21b36a06b6fd2a67
jonathan canaveral <neumerance@live.com>
2020-01-25 16:13:35 +0800

    specs for response to pdf

--- spec/presenters/response_to_pdf_spec.rb
+++ spec/presenters/response_to_pdf_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 describe ResponseToPdf do
+  let(:url) { 'http://www.google.com' }
+  let(:content) { Api2PdfService.generate_pdf_data(url) }
+  let(:file_path) { "tmp/resume-#{content.object_id}.pdf" }
 
+  before do
+    mock_api_2_pdf
+    ResponseToPdf.generate(content: content)
+  end
+
+  after do
+    FileUtils.rm file_path
+  end
+
+  it 'generate a pdf file in tmp folder' do
+    expect(File.file?(file_path)).to be_truthy
+  end
 end
