4cfef749bf38ddc2c5367723368976e3a8d6bc30
jonathan canaveral <neumerance@live.com>
2020-02-04 09:15:47 +0800

    custom page layout on sign in and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   after_action :clear_xhr_flash
 
   layout Proc.new { |controller| controller.request.xhr? ? false : 'application' }
+  layout :layout_by_resource
 
   def clear_xhr_flash
     flash.discard if request.xhr?
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "landing"
+    else
+      "application"
+    end
   end
 end
