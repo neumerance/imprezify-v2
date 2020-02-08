9f0b91088e8ad76defcac9f7eb3dad1833c81914
jonathan canaveral <neumerance@live.com>
2020-02-09 00:18:49 +0800

    fixing bootstrap issue in heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import './src/application.scss'
+import 'packs/src/application.scss'
 
 require("@rails/ujs").start();
 require("turbolinks").start();
