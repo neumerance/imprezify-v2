20fa2ff0ea343813f890647af86f416ae59f330e
jonathan canaveral <neumerance@live.com>
2020-02-13 01:27:56 +0800

    configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,8 @@
 # Change these
-server 'imprezify.com', port: 80, roles: [:web, :app, :db], primary: true
+server 'imprezify.com', user: 'ubuntu', roles: [:web, :app, :db], primary: true
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.6.0@imprezify-v2'
 
 set :repo_url,        'git@github.com:neumerance/imprezify-v2.git'
 set :application,     'imprezify'
@@ -18,7 +21,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -31,7 +34,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
