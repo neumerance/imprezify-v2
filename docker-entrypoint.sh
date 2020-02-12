67c38de9055ac8330f16bf8ebff64492117f79d4
Ubuntu <ubuntu@ip-172-31-16-131.us-east-2.compute.internal>
2020-02-12 00:19:33 +0000

    updating docker entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ rails_env=${RAILS_ENV:-staging}
 
 RAILS_ENV=${rails_env} `which rake` db:create
 RAILS_ENV=${rails_env} `which rake` db:migrate
-RAILS_ENV=${rails_env} `which puma` -C config/puma.rb
+RAILS_ENV=${rails_env} bundle exec rails s
