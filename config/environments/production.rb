e0f472fd54a9e4fd7bd5b7f10c9e397a96406ce4
jonathan canaveral <neumerance@live.com>
2020-02-15 21:16:33 +0800

    configuring S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
+
+  config.active_storage.service = :amazon_s3
 end
